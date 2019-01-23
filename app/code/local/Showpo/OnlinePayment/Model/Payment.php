57a702640a3f6ca0864ec312decf00d7df715ec7
Bin Li <northblue@gmail.com>
2019-01-24 08:30:36 +1100

    Fixed Typo from Comments and removed some redundant code

--- app/code/local/Showpo/OnlinePayment/Model/Payment.php
+++ app/code/local/Showpo/OnlinePayment/Model/Payment.php
@@ -2,11 +2,11 @@
 class Showpo_OnlinePayment_Model_Payment extends Mage_Payment_Model_Method_Abstract{
 
     protected $_code = "showpo_pay";
-
     protected $_canAuthorize = true;
     protected $_canCapture   = true;
 
-    //TODO It should be moved to admin as a configuration value later
+    
+    //TODO- It should be moved to admin as a configuration value later
     protected $url = "www.mocky.io/v2/5c46b1c13100000e0005f401";
     
 
@@ -14,42 +14,30 @@ class Showpo_OnlinePayment_Model_Payment extends Mage_Payment_Model_Method_Abstr
         if(!$quote){
             return false;
         }
-
-        if($quote->getAllVisibleItems() <= 2){
-            return false;
-        }
-
         return true;
     }
     
-    //capture function will make API call and save the response info together with Transaction
+    //Capture function will make API call and save the response info together with Transaction
     public function capture(Varien_Object $payment, $amount)
     {
         $order = $payment->getOrder();
         $result = $this->callApi( $amount = $order->getGrandTotal(), $customerId = $order->getCustomerId(), $reference = $order->getQuoteId());
         
-        Mage::log($result, null, $this->getCode().'.log');
-        if($result === false) {
-            $errorMsg = $this->_getHelper()->__('When Process API Call');
-        } else {
 
-            if($result['status'] == 'Success'){
-                 Mage::log('Showpo Payment - Saving Transaction: '.json_encode($result), null, $this->getCode().'.log');
-                $payment->setTransactionId($result['transaction_id']);
-                $payment->setIsTransactionClosed(1);
-                $payment->setTransactionAdditionalInfo(Mage_Sales_Model_Order_Payment_Transaction::RAW_DETAILS,array('txn_ref'=>$result['transaction_id']));
-            }else{
-                Mage::throwException('API Call Failed');
-            }
+        if($result['status'] == 'Success'){
+             Mage::log('Showpo Payment - Saving Transaction: '.json_encode($result), null, $this->getCode().'.log');
+            $payment->setTransactionId($result['transaction_id']);
+            $payment->setIsTransactionClosed(1);
+            $payment->setTransactionAdditionalInfo(Mage_Sales_Model_Order_Payment_Transaction::RAW_DETAILS,array('txn_ref'=>$result['transaction_id']));
+        }else{
+            Mage::throwException('API Call Failed');
         }
-        if($errorMsg){
-            Mage::throwException($errorMsg);
-        }
+
  
         return $this;
     }
     
-    
+    //Process API call and return info in an array
     private function callApi( $amount,$customerId,$reference){
 
         $dataArray = array("amount"=>$amount, "customer_id"=>$customerId,"reference"=>$reference);
@@ -73,13 +61,12 @@ class Showpo_OnlinePayment_Model_Payment extends Mage_Payment_Model_Method_Abstr
             Mage::log('Showpo Payment - API Response'.$result, null, $this->getCode().'.log');
             curl_close($ch);
             $resultArray = json_decode($result, true);
-;
             return array('status'=>$resultArray['status'],'amount' => $resultArray['amount'] , 'reference' => $resultArray['reference'], 
                 'txn_ref' => $resultArray['txn_ref'],'transaction_id' =>time());
         }
         catch(Exception $e)
         {
-            Mage::throwException(Mage::helper('showpo_onlinepayment')->__('API call failed'));
+            Mage::throwException('API call failed');
             return false;
         }
     }   
